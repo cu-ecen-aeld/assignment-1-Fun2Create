7c6a40b4062fdd792613c5343c2ab7406e1ea004
Fun2Create <Fun2Create@gmx.net>
2024-10-26 19:46:11 +0200

    Added comments

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -3,7 +3,7 @@
 filesdir=$1
 searchstr=$2
 
-if [ $# -eq 2 ];
+if [ $# -eq 2 ];	#ensure correct nr of parameters
 then
 	echo "filesdir = $filesdir and searchstr = ${searchstr}"
 else
@@ -11,13 +11,13 @@ else
 	exit 1
 fi
 
-if [ ! -d $filesdir ]
+if [ ! -d $filesdir ]	#test -d - true only if directory
 then
 	echo "$filesdir is not a directory!"
 	exit 1
 fi
 
-filenumber=$(ls -alR $filesdir | grep ^- | wc -l)
-matchesnumber=$(grep -r $searchstr $filesdir/* | wc -l)
+filenumber=$(ls -alR $filesdir | grep ^- | wc -l)	#ls: -a - all files, -l - long/1 line per file, R - recursive; grep for lines with 1st char "-"; wc -l - count lines
+matchesnumber=$(grep -r $searchstr $filesdir/* | wc -l)	#grep -r - get recursive all lines with string; wc -l - count lines
 echo "The number of files are $filenumber and the number of matching lines are $matchesnumber"
 
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -3,25 +3,25 @@
 writefile=$1
 writestr=$2
 
-if [ $# -ne 2 ];
+if [ $# -ne 2 ];	#request exactly 2 arguments
 then
         echo "expected 2 params, provided $# !"
         exit 1
 fi
 
-#if [ "/" != $(grep -o ^/ <<< ${writefile}) ];
-if [ ! $(grep -o ^/ <<< ${writefile}) ];
+#if [ "/" != $(grep -o ^/ <<< ${writefile}) ];	#alternative to next line, variant with string-compare
+if [ ! $(grep -o ^/ <<< ${writefile}) ];	#input name of writefile (string) to grep, check if it starts with "/" - better alternative to previous line
 then
 	echo "$writefile has no absolute path!"
 	exit 1
 fi	
 
-checkdir=$(grep -o ".*/" <<< $writefile)
-mkdir -p $checkdir
+checkdir=$(grep -o ".*/" <<< $writefile)	#remove filename after last "/" in writefile to store only directory-path in checkdir
+mkdir -p $checkdir				#mkdir -p to create dir only if not already existing, else proceed without error 
 
-$(echo $writestr > $writefile)
+$(echo $writestr > $writefile)			# >- write file, overwrite if it exists
 
-if [ ! -f $writefile ];
+if [ ! -f $writefile ];				# test -f - verify filecreation
 then
 	echo "$writefile could not be written!"
 	exit 1
